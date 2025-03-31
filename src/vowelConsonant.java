e520fbf57751fe9edb58cef7cb4bbd7a2b6d7020
vineet-thyagaraj <vineetgowda.thyagaraj@tekeinstein.com>
2025-03-31 14:43:47 -0400

    first commit

---
+++ src/vowelConsonant.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class vowelConsonant {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a letter: ");
+        String letter =  scanner.nextLine();
+
+        if(letter.length() == 1 && letter.matches("[a-zA-Z]+")){
+            String l = letter.toUpperCase();
+            if((l.endsWith("A")) || (l.equals("E")) || (l.equals("I")) || (l.equals("O")) || (l.equals("U"))){
+                System.out.println(letter + " is a vowel");
+            }else{
+                System.out.println(letter + " is a consonant");
+            }
+        } else {
+            System.out.println(letter + " is an invalid input");
+        }
+    }
+}
